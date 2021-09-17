6ca8201dc180ece6073af49e96aeeb623f03e666
LandonBecker <landonbecker123@gmail.com>
2021-09-17 08:05:05 -0600

    Update to database migration

--- back-end/database_migration.py
+++ back-end/database_migration.py
@@ -36,3 +36,28 @@ def merge0_0(DATABASE):
     connection.close()
 
     return "Merged Successfully"
+
+def debug_tables(DATABASE):
+    # Create a connection object
+    connection  = sqlite3.connect(DATABASE)
+
+    # Get a cursor
+    cursor      = connection.cursor()
+
+    # Query the SQLite master table
+    tableQuery = "select * from sqlite_master"
+    cursor.execute(tableQuery)
+    tableList = cursor.fetchall()
+
+    # Print the updated listed of tables after renaming the stud table
+    for table in tableList:
+        print("Database Object Type: %s"%(table[0]))
+        print("Name of the database object: %s"%(table[1]))
+        print("Name of the table: %s"%(table[2]))
+        print("Root page: %s"%(table[3]))
+        print("SQL Statement: %s"%(table[4]))
+
+    # close the database connection
+    connection.close()
+
+    return "Merged Successfully"
