c3dff6ff4e493b1cc5d65f41e8e9a2b8175b614a
LandonBecker <landonbecker123@gmail.com>
2021-10-05 09:37:59 -0600

    Create database if not exists

--- back-end/database_initiation.py
+++ back-end/database_initiation.py
@@ -3,14 +3,11 @@ import os
 from models import *
 from sqlalchemy import *
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy_utils import database_exists, create_database, drop_database
 from database_migration import *
 
 ROOT = os.path.dirname(os.path.abspath(__file__))
 DATABASE = "sqlite:///{}".format(os.path.join(ROOT, 'users', "Phlash.db"))
 engine = create_engine(DATABASE)
-if not database_exists(engine.url):
-    create_database(engine.url)
 Session = sessionmaker(bind=engine)
 session = Session()
 print_tables(os.path.join(ROOT, 'users', "Phlash.db"))
